0ecf928db45f9f670d95621fb33f9e6d603f8be2
TJHL <DONT CHANGE THIS!!!!!>
2017-01-09 18:17:08 -0800

    Working Start

--- src/BrownieTapperLauncher.java
+++ src/BrownieTapperLauncher.java
@@ -1,4 +1,30 @@
+import javax.swing.JFrame;
+
 
 public class BrownieTapperLauncher {
-
+	
+	public static void main(String[] args) {
+		
+		BrownieTapperLauncher a = new BrownieTapperLauncher();
+	}
+	
+	JFrame frame;
+	GamePanel panel;
+	static final int WIDTH=1000;
+	static final int HEIGHT=1000;
+	
+	BrownieTapperLauncher(){
+		frame = new JFrame();
+		panel = new GamePanel();
+		setup();
+	}
+		void setup(){
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.startGame();
+		}
+	
+		
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
+public class GamePanel extends JPanel implements ActionListener {
+	Timer clock;
+	
+	GamePanel(){
+	
+		clock = new Timer(1000 / 60,(ActionListener) this);
+	}
+		void startGame(){
+			clock.start();
+	
+		}
+
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
 }
